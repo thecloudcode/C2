28cc7cd39202419b8e92138c5e7d8f58e4607e5d
Badal Prasad Singh <officialbadalprasadsingh@gmail.com>
2023-09-29 22:40:37 +0530

    Cool

---
+++ _next/static/aaCyMHnPqoY-6q3BJj2or/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-2e9a3420273896f4.js","static/chunks/pages/index-a2c52b43209ae81e.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
